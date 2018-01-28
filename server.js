606fce5222e49a54acfe819cac72bb160f22637e
agolumbovsky <agolumbovsky@gmail.com>
2018-01-28 14:58:41 -0500

    serial write is working

--- server.js
+++ server.js
@@ -1,20 +1,56 @@
-const express = require('express');
-
-const SerialPort = require('serialport');
+/* const SerialPort = require('serialport');
 
 console.log("start of program")
 
+const portName = 'COM2'
 console.log(portName);
+
 var myPort = new SerialPort(portName, 9600);
+
+myPort.on('open', (myPort) => {
+    console.log("something is open")
+});
+
+myPort.write("I have no idea wha't going on...")
 
 // list serial ports:
 SerialPort.list(function (err, ports) {
  ports.forEach(function(port) {
-   console.log(port.comName);
+   console.log("the port com name is", port.comName);
  });
 });
-setTimeout(function() {
-    console.log("timing out for one second")
-}, 1000);
 
-console.log("end of program")
+console.log("end of program") */
+
+
+
+const SerialPort = require('serialport');
+
+const port = new SerialPort('COM3', (err) => {
+	
+	if(err) console.log("Error:", err.message);
+	
+});
+
+// console.log("the port is:", port);
+
+port.on('open', () => {
+	console.log("port is open for business");
+});
+
+port.write("hoopla", (err) => {
+	
+	if(err) console.log("Error while writing:", err.message);
+	console.log("message written successfully");
+});
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
