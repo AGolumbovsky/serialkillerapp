761cbee67cb696839a83d846650564b05e7c1976
Andriy Golumbovsky <AGolumbovsky@gmail.com>
2018-05-22 08:48:34 -0400

    catch up

--- server.js
+++ server.js
@@ -3,11 +3,9 @@ const SerialPort	= require('serialport');
 
 const app = express();
 
-const httpPort = process.env.PORT || 8787;
-
-app.use(express.static(__dirname + '/public'));
-
-const serPort = new SerialPort('COM3', (err) => {
+// Serial business
+// ***************
+const serPort = new SerialPort('COM5', (err) => {
 	
 	if(err) console.log("Error creating new port:", err.message);
 	
@@ -17,13 +15,18 @@ const serPort = new SerialPort('COM3', (err) => {
 
 
 
-var serPorts = ["at first index", "at second index"]; // secretaty Not Sure
+var serPorts = ["at first index", "at second index"]; // Secretary Not Sure
 
 serPort.on('open', () => {
 
 	console.log("port is open for business");
 
 });
+
+serPort.on('data', (data) => {
+
+	console.log(data)
+})
 
 // start of weird code that may (most likely will) cause problems and shit
 // *** *** *** 
@@ -32,27 +35,49 @@ SerialPort.list(function (err, serPorts) {
 
 	if(err) console.log("some shit happened:", err.message);
 
-	console.log("look, i'm logging something inside SerialPort.list()")
-
-	serPorts.forEach(function(port) {
-
-	  console.log("the port com name is", serPort.comName);
-
-	});
+	else {
+		
+		
+		console.log("listing ports")
+		
+		serPorts.forEach(function(port) {
+			
+			console.log("the port com name is", serPort.comName);
+			
+		});
+	}
 });
 
 console.log("middle of the file, checking if I even got to this point");
-// *** *** *** 
-// *** *** *** ***
-// end of weird code that might have(most likely did) caused problems and shit
 
-serPort.write("hoopla", (err) => {
+serPort.write("RESTART 0 DEVICE<LF>", (err) => { // string here is from Tesira Forte
 	
 	if(err) console.log("Errr, there was error writing:", err.message);
 
-	console.log("message written successfully");
+	else {
+
+		console.log("message written successfully");
+	}
 });
 
+
+// HTTP business
+// *************
+
+const httpPort = process.env.PORT || 8787;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('getAll', (req, res) => {
+
+	console.log("testing sending stuff...");
+
+	res.send("testing app.get in server.js");
+
+})
+
 app.listen(httpPort, () => {
+
 	console.log("\n web server listening on port", httpPort);
+
 })
